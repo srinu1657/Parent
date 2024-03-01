8ba3fb44e098842afd1c0748561bed98010e2d04
srinivas samala <srinu1657@gmail.com>
2024-03-01 02:37:28 -0600

    master changes

--- src/main/java/org/parent/example/beans/B.java
+++ src/main/java/org/parent/example/beans/B.java
@@ -2,8 +2,8 @@ package org.parent.example.beans;
 
 public class B {
     public void display(){
-        System.out.println("feature1");
+        System.out.println("master1");
         System.out.println("master2");
-        System.out.println("feature3");
+        System.out.println("master3");
     }
 }
--- src/main/java/org/parent/example/beans/C.java
+++ src/main/java/org/parent/example/beans/C.java
@@ -5,9 +5,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class C {
     public void display(){
-        System.out.println("feature1");
-        System.out.println("feature2");
-        System.out.println("feature3");
+        System.out.println("master1");
+        System.out.println("master2");
+        System.out.println("master3");
     }
 
 }
