890ea22ffc4ac5150ee1d14daf588e465729c6ef
srinivas samala <srinu1657@gmail.com>
2024-03-01 01:46:46 -0600

    master 1 in main

--- src/main/java/org/parent/example/beans/C.java
+++ src/main/java/org/parent/example/beans/C.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class C {
     public void display(){
-        System.out.println("srinivas");
-        System.out.println("master2");
+        System.out.println("master1");
+        System.out.println("master1");
     }
 
 }
